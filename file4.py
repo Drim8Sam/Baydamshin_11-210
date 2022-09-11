8756c6d2308a2b29b6e3f15fa2ccf89f239d0172
Baydamshin <baydamshin.r@mail.ru>
2022-09-11 14:58:15 +0300

    first

---
+++ file4.py
@@ -0,0 +1,1 @@
+text = 'ывапролорпавыкуенгрпекыуклганпоьавркыенолгапоыркеунолгепаыеуноклгепствыреункгелапыеруонк'statistic = dict()for i in text:    if i.isalpha():        if i.lower() not in statistic.keys():            statistic[i.lower()] = 1        else:            statistic[i.lower()] += 1for key in statistic.keys():    print(key, '-', statistic[key])
