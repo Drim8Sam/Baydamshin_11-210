c6798aec4dcf0fd2dce46513e99e56bf60f118bc
Ренат Байдамшин <112691393+Drim8Sam@users.noreply.github.com>
2022-10-18 16:52:41 +0300

    v6.1.1

--- ДЗ_№6/6.1.py
+++ ДЗ_№6/6.1.py
@@ -1,24 +1,26 @@
 text = input()
+
 while text != 'quit':
+    
     count = 1
     symbol = ''
+    
     for i in text:
         if i == symbol:
             count += 1
         else:
             if symbol:
                 print(symbol, end='')
+                
                 if count > 1:
                     print(count, end='')
+            
             symbol = i
             count = 1
+    
     if symbol:
         print(symbol, end='')
         if count > 1:
             print(count, end='')
 
     text = input()
-
-
-
-
