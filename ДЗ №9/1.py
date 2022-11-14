7e9ba1ad04254d4a81d02cfa07d5d5e479a0ef43
Ренат Байдамшин <112691393+Drim8Sam@users.noreply.github.com>
2022-11-14 23:46:20 +0300

    v9.1.1

--- ДЗ №9/1.py
+++ ДЗ №9/1.py
@@ -1,1 +1,10 @@
-#1. На вход подается список из чисел. Вывести результат произведения всех положительный чисел.numbers = [1, 5, 4, 7, 12, 8, 9, -6]even_ones_numbers = list(filter(lambda x: x % 2 == 0 and x > 0, numbers))count = 1for i in even_ones_numbers:    count *= iprint(count)
+#1. На вход подается список из чисел. Вывести результат произведения всех положительный чисел.
+
+numbers = [1, 5, 4, 7, 12, 8, 9, -6]
+even_ones_numbers = list(filter(lambda x: x % 2 == 0 and x > 0, numbers))
+count = 1
+
+for i in even_ones_numbers:
+    count *= i
+    
+print(count)
