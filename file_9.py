843bf02864bd6d3ab61c60a8c9eb6023db5b4df5
Ренат Байдамшин <112691393+Drim8Sam@users.noreply.github.com>
2023-03-31 20:23:30 +0300

    Update file_9.py

--- file_9.py
+++ file_9.py
@@ -1,1 +1,12 @@
-number = int(input('Введите число: '))base = 2numberj = ''while number > 0:    numberj += str(number % base)    number = number // basenumberj = numberj[::-1]print(numberj)
+number = int(input('Введите число: '))
+base = 2
+numberj = ''
+
+while number > 0:
+    numberj += str(number % base)
+    number = number // base
+
+numberj = numberj[::-1]
+
+
+print(numberj)
